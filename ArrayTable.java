73ce1fb97d8e882a5f55d5d8f333e901e8980ef8
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-01-23 19:44:29 +0530

    Array Table
    Array Table

---
+++ ArrayTable.java
@@ -0,0 +1,16 @@
+
+package arraytable;
+
+
+public class ArrayTable {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Index\tValue");
+        int array[] = {12,46,32,28,79};
+        for(int i=0;i<array.length;i++){
+            System.out.println(i+"\t"+array[i]);
+        }
+    }
+    
+}
